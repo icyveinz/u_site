f4dca7a48a521923d9f6304287fbc81558838da8
Egor Ivanov <rust_lang_lover@proton.me>
2024-03-18 14:22:38 +0300

    //

--- src/js/js_scripts/visual_activators.js
+++ src/js/js_scripts/visual_activators.js
@@ -1,14 +1,14 @@
 $(function() {
     AOS.init(); // activates AOS for smooth scroll appearance
     new WOW().init(); // activates wow for tracking when on screen
-    $(window).scroll(function(){ // activate arrow for quick scroll to the top
-        if ($(this).scrollTop() > 600) {
-            $('.pageup-navigation').fadeIn();
-        }
-        else {
-            $('.pageup-navigation').fadeOut();
-        }
-    });
+    // $(window).scroll(function(){ // activate arrow for quick scroll to the top
+    //     if ($(this).scrollTop() > 600) {
+    //         $('.pageup-navigation').fadeIn();
+    //     }
+    //     else {
+    //         $('.pageup-navigation').fadeOut();
+    //     }
+    // }); // Disabled, activate if needed.
 
     $("a[href^='#']").click(function() { // For a smooth scroll to the top of the page.
         const _href = $(this).attr("href");
