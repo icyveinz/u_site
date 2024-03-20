be950f3006cc68f51ced6938a098e36fa4ee7c27
rust_lang_lover <rust_lang_lover@proton.me>
2024-03-20 15:07:04 +0300

    //

--- src/js/js_scripts/burger_menu.js
+++ src/js/js_scripts/burger_menu.js
@@ -1,1 +1,39 @@
+$(function() {
+    $('.box-burger-mobile-container__button').on('click', function() {
+        if (document.querySelectorAll('.button-holder-href-container').length > 0) {
+            document.querySelectorAll('.button-holder-href-container').forEach(function (object) {
+                object.classList.add('animate__fadeOutUp');
+                function handle_removing(event) {
+                    event.stopPropagation();
+                    object.remove();
+                }
+                object.addEventListener('animationend', handle_removing, {once : true});
+            })
+        }
+        else {
+            return_button_objects().forEach(function (object) {
+                $('.mobile-menu-grid-container').append(object);
+            });
+        }
+    });
+})
 
+function return_button_objects() {
+    return [
+        `<div class="button-holder-href-container animate__animated animate__backInLeft">
+                    <a class="button-holder-href-container__a" href="#production-anchor">
+                        Продукция
+                    </a>
+                </div>`,
+        `<div class="button-holder-href-container animate__animated animate__backInLeft">
+                    <a class="button-holder-href-container__a" href="#principles-anchor">
+                        Принципы
+                    </a>
+                </div>`,
+        `<div class="button-holder-href-container animate__animated animate__backInLeft">
+                    <a class="button-holder-href-container__a" href="#contacts-anchor">
+                        Контакты
+                    </a>
+                </div>`
+    ]
+}
