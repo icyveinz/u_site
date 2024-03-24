53fcba4f33f06aa4a732319d2f542625fb33786b
Egor Ivanov <rust_lang_lover@proton.me>
2024-03-24 12:44:38 +0300

    // Main menu - mobile version optimizing remaining

--- dist/js/js_scripts/banner_logic.js
+++ dist/js/js_scripts/banner_logic.js
@@ -1,19 +1,8 @@
-function user_date_formatted() {
-    const date = new Date();
-    return `${date.getDate()}:${date.getMonth()}:${date.getFullYear()}`
-}
-
 $(function() {
     $('.buttons-holder-container__button-enter').on('click', function() {
-        localStorage.setItem('ugo-authorized', user_date_formatted());
         window.location.href = 'presentation.html'
     });
     $('.buttons-holder-container__button-leave').on('click', function() {
         window.location.href = 'https://minzdrav.gov.ru/documents/5736-federalnyy-zakon-15-fz-ot-23-fevralya-2013-g'
-    })
+    });
 })
-
-function check_redirect() {
-    const current_date = new Date();
-    const confirm_date = localStorage.getItem('ugo-authorized');
-}
