3acddca1b0df31e6aca253ae8f99c250bb53d862
rust_lang_lover <rust_lang_lover@proton.me>
2024-03-25 14:58:10 +0300

    // Added phone instead instagram icon

--- dist/js/js_scripts/main_styler.js
+++ dist/js/js_scripts/main_styler.js
@@ -33,14 +33,13 @@ function link_hover_trackers() {
     $('.logo-link-holder').on('mouseenter', function() {
         console.log("Hovered link");
         $(this).find('#telegram-icon').css("filter", "brightness(0) saturate(100%) invert(57%) sepia(68%) saturate(688%) hue-rotate(167deg) brightness(92%) contrast(90%)");
-        $(this).find('#instagram-icon').css("filter", "brightness(0) saturate(100%) invert(34%) sepia(27%) saturate(4325%) hue-rotate(307deg) brightness(90%) contrast(84%)")
         $(this).find('.logo-link-holder__a').css("filter", "brightness(0) saturate(100%) invert(100%) sepia(0%) saturate(7498%) hue-rotate(38deg) brightness(93%) contrast(110%)");
-        $(this).find('.logo-link-holder__a-mail').css("filter", "brightness(0) saturate(100%) invert(100%) sepia(0%) saturate(7498%) hue-rotate(38deg) brightness(93%) contrast(110%)");
+        $(this).find('.logo-link-holder__a-additional').css("filter", "brightness(0) saturate(100%) invert(100%) sepia(0%) saturate(7498%) hue-rotate(38deg) brightness(93%) contrast(110%)");
     });
     $('.logo-link-holder').on('mouseleave', function() {
         $(this).find('.logo-link-holder__img').css("filter", "brightness(0) saturate(100%) invert(64%) sepia(7%) saturate(16%) hue-rotate(317deg) brightness(86%) contrast(86%)");
         $(this).find('.logo-link-holder__a').css("filter", "brightness(0) saturate(100%) invert(64%) sepia(7%) saturate(16%) hue-rotate(317deg) brightness(86%) contrast(86%)");
-        $(this).find('.logo-link-holder__a-mail').css("filter", "brightness(0) saturate(100%) invert(64%) sepia(7%) saturate(16%) hue-rotate(317deg) brightness(86%) contrast(86%)");
+        $(this).find('.logo-link-holder__a-additional').css("filter", "brightness(0) saturate(100%) invert(64%) sepia(7%) saturate(16%) hue-rotate(317deg) brightness(86%) contrast(86%)");
     })
 }
 
