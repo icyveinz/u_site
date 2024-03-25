7dd039e958194ccaffef020582c8bba34e8d8d33
rust_lang_lover <rust_lang_lover@proton.me>
2024-03-25 15:06:24 +0300

    // Added animated filter transition to @ugo_bar under Socials

--- src/js/js_scripts/main_styler.js
+++ src/js/js_scripts/main_styler.js
@@ -40,7 +40,13 @@ function link_hover_trackers() {
         $(this).find('.logo-link-holder__img').css("filter", "brightness(0) saturate(100%) invert(64%) sepia(7%) saturate(16%) hue-rotate(317deg) brightness(86%) contrast(86%)");
         $(this).find('.logo-link-holder__a').css("filter", "brightness(0) saturate(100%) invert(64%) sepia(7%) saturate(16%) hue-rotate(317deg) brightness(86%) contrast(86%)");
         $(this).find('.logo-link-holder__a-additional').css("filter", "brightness(0) saturate(100%) invert(64%) sepia(7%) saturate(16%) hue-rotate(317deg) brightness(86%) contrast(86%)");
-    })
+    });
+    $('.legal-content-holder-vertical-layout__shadowed-big').on('mouseenter', function() {
+        $(this).css("filter", "brightness(0) saturate(100%) invert(100%) sepia(0%) saturate(7498%) hue-rotate(38deg) brightness(93%) contrast(110%)");
+    });
+    $('.legal-content-holder-vertical-layout__shadowed-big').on('mouseleave', function() {
+        $(this).css("filter", "brightness(0) saturate(100%) invert(64%) sepia(7%) saturate(16%) hue-rotate(317deg) brightness(86%) contrast(86%)");
+    });
 }
 
 function ugo_black_exclusion(source_value) {
