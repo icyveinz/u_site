a15d47938ad1d08f1cb3336280cb8fb9d04256a1
rust_lang_lover <rust_lang_lover@proton.me>
2024-03-28 12:52:04 +0300

    // Added yandex metrix + google metrics

---
+++ dist/js/js_scripts/bar-loader-min.js
@@ -0,0 +1,1 @@
+$(function(){$(document.body).css({height:"100%","overflow-y":"hidden"}),$(document).ready(function(){$(document.body).css("overflow-y","auto"),setTimeout(function(){$(".preloader-overlay, .preloader-main").fadeOut(250,function(){$(".preloader-overlay, .preloader-main").remove()})},250)})});
