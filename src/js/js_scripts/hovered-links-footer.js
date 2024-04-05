96e070b98a9349f9bf79600c49458114552b8ed3
Egor Ivanov <rust_lang_lover@proton.me>
2024-04-05 12:15:21 +0300

    // added user agreement page + moved footer hover trackers in hovered-links-footer.js

---
+++ src/js/js_scripts/hovered-links-footer.js
@@ -0,0 +1,23 @@
+function link_hover_trackers() { // Activator of animations for information in footer
+    $('.logo-link-holder').on('mouseenter', function() {
+        console.log("Hovered link");
+        $(this).find('#telegram-icon').css("filter", "brightness(0) saturate(100%) invert(57%) sepia(68%) saturate(688%) hue-rotate(167deg) brightness(92%) contrast(90%)");
+        $(this).find('.logo-link-holder__a').css("filter", "brightness(0) saturate(100%) invert(100%) sepia(0%) saturate(7498%) hue-rotate(38deg) brightness(93%) contrast(110%)");
+        $(this).find('.logo-link-holder__a-additional').css("filter", "brightness(0) saturate(100%) invert(100%) sepia(0%) saturate(7498%) hue-rotate(38deg) brightness(93%) contrast(110%)");
+    });
+    $('.logo-link-holder').on('mouseleave', function() {
+        $(this).find('.logo-link-holder__img').css("filter", "brightness(0) saturate(100%) invert(64%) sepia(7%) saturate(16%) hue-rotate(317deg) brightness(86%) contrast(86%)");
+        $(this).find('.logo-link-holder__a').css("filter", "brightness(0) saturate(100%) invert(64%) sepia(7%) saturate(16%) hue-rotate(317deg) brightness(86%) contrast(86%)");
+        $(this).find('.logo-link-holder__a-additional').css("filter", "brightness(0) saturate(100%) invert(64%) sepia(7%) saturate(16%) hue-rotate(317deg) brightness(86%) contrast(86%)");
+    });
+    $('.legal-content-holder-vertical-layout__shadowed-big').on('mouseenter', function() {
+        $(this).css("filter", "brightness(0) saturate(100%) invert(100%) sepia(0%) saturate(7498%) hue-rotate(38deg) brightness(93%) contrast(110%)");
+    });
+    $('.legal-content-holder-vertical-layout__shadowed-big').on('mouseleave', function() {
+        $(this).css("filter", "brightness(0) saturate(100%) invert(64%) sepia(7%) saturate(16%) hue-rotate(317deg) brightness(86%) contrast(86%)");
+    });
+}
+
+$(function() {
+    link_hover_trackers();
+})
--- src/js/js_scripts/main_styler.js
+++ src/js/js_scripts/main_styler.js
@@ -39,26 +39,6 @@ function release_animated_text(index) { // -> String || Text for photo slider
     }
 }
 
-function link_hover_trackers() { // Activator of animations for information in footer
-    $('.logo-link-holder').on('mouseenter', function() {
-        console.log("Hovered link");
-        $(this).find('#telegram-icon').css("filter", "brightness(0) saturate(100%) invert(57%) sepia(68%) saturate(688%) hue-rotate(167deg) brightness(92%) contrast(90%)");
-        $(this).find('.logo-link-holder__a').css("filter", "brightness(0) saturate(100%) invert(100%) sepia(0%) saturate(7498%) hue-rotate(38deg) brightness(93%) contrast(110%)");
-        $(this).find('.logo-link-holder__a-additional').css("filter", "brightness(0) saturate(100%) invert(100%) sepia(0%) saturate(7498%) hue-rotate(38deg) brightness(93%) contrast(110%)");
-    });
-    $('.logo-link-holder').on('mouseleave', function() {
-        $(this).find('.logo-link-holder__img').css("filter", "brightness(0) saturate(100%) invert(64%) sepia(7%) saturate(16%) hue-rotate(317deg) brightness(86%) contrast(86%)");
-        $(this).find('.logo-link-holder__a').css("filter", "brightness(0) saturate(100%) invert(64%) sepia(7%) saturate(16%) hue-rotate(317deg) brightness(86%) contrast(86%)");
-        $(this).find('.logo-link-holder__a-additional').css("filter", "brightness(0) saturate(100%) invert(64%) sepia(7%) saturate(16%) hue-rotate(317deg) brightness(86%) contrast(86%)");
-    });
-    $('.legal-content-holder-vertical-layout__shadowed-big').on('mouseenter', function() {
-        $(this).css("filter", "brightness(0) saturate(100%) invert(100%) sepia(0%) saturate(7498%) hue-rotate(38deg) brightness(93%) contrast(110%)");
-    });
-    $('.legal-content-holder-vertical-layout__shadowed-big').on('mouseleave', function() {
-        $(this).css("filter", "brightness(0) saturate(100%) invert(64%) sepia(7%) saturate(16%) hue-rotate(317deg) brightness(86%) contrast(86%)");
-    });
-}
-
 function ugo_black_exclusion(source_value) { // -> bool || If image is ugo_black -> no converting
     if (source_value === '../assets/images/ugo_black%20copy.webp' || source_value === '../assets/images/ugo_black%20copy%202.webp') {
         return true
@@ -134,5 +114,4 @@ $(function() {
     place_items();
     track_slides();
     track_hovers();
-    link_hover_trackers();
 })
