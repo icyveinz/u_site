f9966df4300c4c2d224cb25de8db52d8d492ab9d
Egor Ivanov <rust_lang_lover@proton.me>
2024-04-15 19:15:30 +0300

    // removed hover trackers

--- src/js/js_scripts/hover_route_getter.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    const scroll_segment = sessionStorage.getItem("presentation-scroll");
-    if (scroll_segment !== null) {
-        console.log(scroll_segment);
-        document.getElementById(scroll_segment).scrollIntoView({behavior : 'smooth'});
-        sessionStorage.setItem("presentation-scroll", null);
-    }
-})
