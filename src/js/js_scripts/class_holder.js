b0e3fb779becf3dbd3d97166cf99d78dc073acf5
rust_lang_lover <rust_lang_lover@proton.me>
2024-04-16 11:43:40 +0300

    // Added svg-packs

--- src/js/js_scripts/class_holder.js
+++ src/js/js_scripts/class_holder.js
@@ -1,13 +1,13 @@
 class CardBuilder {
     constructor() {
         this.storage = [
-            {'name' : 'UGOBAR 1500 YELLOW', 'photo' : '../assets/images/ugo_yellow%20copy.webp', 'classname' : 'ugo-naming__yellow', 'taste' : 'Спелое манго'},
-            {'name' : 'UGOBAR 1500 GREEN', 'photo' : '../assets/images/ugo_green%20copy.webp', 'classname' : 'ugo-naming__green', 'taste' : 'Прохладный арбуз'},
-            {'name' : 'UGOBAR 1500 BLACK', 'photo' : '../assets/images/ugo_black%20copy.webp', 'classname' : 'ugo-naming__black', 'taste' : 'Кола со льдом'},
-            {'name' : 'UGOBAR 1500 PINK', 'photo' : '../assets/images/ugo_pink%20copy.webp', 'classname' : 'ugo-naming__pink', 'taste' : 'Ягодный лимонад'},
-            {'name' : 'UGOBAR 1500 BLUE', 'photo' : '../assets/images/ugo_blue%20copy.webp', 'classname' : 'ugo-naming__blue', 'taste' : 'Голубика со льдом'},
-            {'name' : 'UGOBAR 1500 RED', 'photo' : '../assets/images/ugo_red%20copy.webp', 'classname' : 'ugo-naming__red', 'taste' : 'Красное яблоко'},
-            {'name' : 'UGOBAR 1500 VIOLET', 'photo' : '../assets/images/ugo_purple%20copy.webp', 'classname' : 'ugo-naming__violet', 'taste' : 'Черника с вишней'}
+            {'name' : 'UGOBAR 1500 YELLOW', 'photo' : '../assets/images/yellow_web.svg', 'classname' : 'ugo-naming__yellow', 'taste' : 'Спелое манго'},
+            {'name' : 'UGOBAR 1500 GREEN', 'photo' : '../assets/images/green_web.svg', 'classname' : 'ugo-naming__green', 'taste' : 'Прохладный арбуз'},
+            {'name' : 'UGOBAR 1500 BLACK', 'photo' : '../assets/images/black_web.svg', 'classname' : 'ugo-naming__black', 'taste' : 'Кола со льдом'},
+            {'name' : 'UGOBAR 1500 PINK', 'photo' : '../assets/images/pink_web.svg', 'classname' : 'ugo-naming__pink', 'taste' : 'Ягодный лимонад'},
+            {'name' : 'UGOBAR 1500 BLUE', 'photo' : '../assets/images/blue_web.svg', 'classname' : 'ugo-naming__blue', 'taste' : 'Голубика со льдом'},
+            {'name' : 'UGOBAR 1500 RED', 'photo' : '../assets/images/red_web.svg', 'classname' : 'ugo-naming__red', 'taste' : 'Красное яблоко'},
+            {'name' : 'UGOBAR 1500 VIOLET', 'photo' : '../assets/images/violet_web.svg', 'classname' : 'ugo-naming__violet', 'taste' : 'Черника с вишней'}
         ];
     }
 
