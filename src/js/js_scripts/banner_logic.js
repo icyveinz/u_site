d5210c1b198941b0edccbc5e65fd9a78151cd54f
Egor Ivanov <rust_lang_lover@proton.me>
2024-06-03 13:36:20 +0300

    // if age is confirmed => redirect to the presentation page immediately

--- src/js/js_scripts/banner_logic.js
+++ src/js/js_scripts/banner_logic.js
@@ -5,18 +5,23 @@ function ageAuthorizedSetLocal() {
 
 // Script for visuals at the entry page.
 $(function() {
-    $('.buttons-holder-container__button-enter').on('click', function() {
-        ageAuthorizedSetLocal();
-        window.location.href = 'presentation'
-    });
-    $('.buttons-holder-container__button-leave').on('click', function() {
-        window.location.href = 'https://minzdrav.gov.ru/documents/5736-federalnyy-zakon-15-fz-ot-23-fevralya-2013-g'
-    });
-    $('.buttons-holder-flex__button-enter').on('click', function() {
-        ageAuthorizedSetLocal();
-        window.location.href = 'presentation'
-    });
-    $('.buttons-holder-flex__button-leave').on('click', function() {
-        window.location.href = 'https://minzdrav.gov.ru/documents/5736-federalnyy-zakon-15-fz-ot-23-fevralya-2013-g'
-    })
+    if (localStorage.getItem("age-auth") === "true") {
+        window.location.replace("presentation")
+    }
+    else {
+        $('.buttons-holder-container__button-enter').on('click', function () {
+            ageAuthorizedSetLocal();
+            window.location.href = 'presentation'
+        });
+        $('.buttons-holder-container__button-leave').on('click', function () {
+            window.location.href = 'https://google.com'
+        });
+        $('.buttons-holder-flex__button-enter').on('click', function () {
+            ageAuthorizedSetLocal();
+            window.location.href = 'presentation'
+        });
+        $('.buttons-holder-flex__button-leave').on('click', function () {
+            window.location.href = 'https://google.com'
+        });
+    }
 })
