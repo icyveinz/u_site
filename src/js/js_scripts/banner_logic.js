95020ad08e6b14f5b7d2b462ec84c22263400532
Egor Ivanov <rust_lang_lover@proton.me>
2024-06-03 13:30:47 +0300

    // if age is not confirmed - return to the start page => done

--- src/js/js_scripts/banner_logic.js
+++ src/js/js_scripts/banner_logic.js
@@ -1,12 +1,19 @@
+// Function for setting the age of user as confirmed to visit other pages
+function ageAuthorizedSetLocal() {
+    localStorage.setItem("age-auth", "true")
+}
+
 // Script for visuals at the entry page.
 $(function() {
     $('.buttons-holder-container__button-enter').on('click', function() {
+        ageAuthorizedSetLocal();
         window.location.href = 'presentation'
     });
     $('.buttons-holder-container__button-leave').on('click', function() {
         window.location.href = 'https://minzdrav.gov.ru/documents/5736-federalnyy-zakon-15-fz-ot-23-fevralya-2013-g'
     });
     $('.buttons-holder-flex__button-enter').on('click', function() {
+        ageAuthorizedSetLocal();
         window.location.href = 'presentation'
     });
     $('.buttons-holder-flex__button-leave').on('click', function() {
