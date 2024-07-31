84ea687d2ac8f6d5659a113d9c316f1ed4efe8b0
rust_lang_lover <rust_lang_lover@proton.me>
2024-07-31 12:37:13 +0300

    changed animation

--- src/js/js_scripts/legal_gsap_animation.js
+++ src/js/js_scripts/legal_gsap_animation.js
@@ -1,22 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
-    gsap.from('.user-agreement-text__header', {
-        opacity: 0,
-        yPercent: -15,
-        duration: 1
-    });
-    const listing = document.querySelectorAll('.user-agreement-additional-layout');
-    listing.forEach((value) => {
-        const timeline = gsap.timeline();
-        timeline
-            .from(value.querySelector('h4'), {
-                opacity: 0,
-                xPercent: -10,
-                duration: 2
-            })
-            .from(value.querySelector('h5'), {
-                opacity: 0,
-                xPercent: -10,
-                duration: 1
-            });
-    });
-})
+    const refactored = gsap.timeline();
+    refactored
+        .from('.user-agreement-text__header', {
+            opacity: 0,
+            yPercent: -15,
+            duration: 1
+        })
+        .from('.user-agreement-text__paragraph', {
+            opacity: 0,
+            xPercent: -10,
+            duration: 2,
+            stagger: 0.1
+        })
+        .from('.user-agreement-text__information', {
+            opacity: 0,
+            xPercent: -10,
+            duration: 1,
+            stagger: 0.2
+        });
+});
+
+// Another type of animation
+// gsap.from('.user-agreement-text__header', {
+//     opacity: 0,
+//     yPercent: -15,
+//     duration: 1
+// });
+// const listing = document.querySelectorAll('.user-agreement-additional-layout');
+// listing.forEach((value) => {
+//     const timeline = gsap.timeline();
+//     timeline
+//         .from(value.querySelector('h4'), {
+//             opacity: 0,
+//             xPercent: -10,
+//             duration: 2
+//         })
+//         .from(value.querySelector('h5'), {
+//             opacity: 0,
+//             xPercent: -10,
+//             duration: 1
+//         });
+// });
