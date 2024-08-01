a83c2eafd433bd2f29d6f49c0920e8cbd2e18d2d
rust_lang_lover <rust_lang_lover@proton.me>
2024-08-01 13:45:11 +0300

    final variant of animation for legal_gsap_animation.js

--- src/js/js_scripts/legal_gsap_animation.js
+++ src/js/js_scripts/legal_gsap_animation.js
@@ -1,37 +1,25 @@
 gsap.registerPlugin(ScrollTrigger);
 
 document.addEventListener('DOMContentLoaded', () => {
-    gsap.from('.user-agreement-text__header', {
-        opacity: 0,
-        yPercent: -15,
-        duration: 1
-    });
-    const segment_trigger = document.querySelectorAll('.user-agreement-additional-layout');
-    segment_trigger.forEach((segment) => {
-        const single_timeline = gsap.timeline({
-            scrollTrigger: {
-                trigger: segment,
-                start: 'top bottom',
-                scrub: true,
-                markers: true,
-                end: 'bottom bottom',
-                toggleActions: 'play none none reverse'
-            }
+    const legal_line = gsap.timeline();
+    legal_line
+        .from('.user-agreement-text__header', {
+            opacity: 0,
+            yPercent: -15,
+            duration: 1
+        })
+        .from('.user-agreement-text__paragraph', {
+            opacity: 0,
+            xPercent: -10,
+            duration: 2,
+            stagger: 0.1
+        })
+        .from('.user-agreement-text__information', {
+            opacity: 0,
+            xPercent: -10,
+            duration: 1,
+            stagger: 0.2
         });
-        single_timeline
-            .from(segment.querySelector('.user-agreement-text__paragraph'), {
-                opacity: 0,
-                xPercent: -10,
-                duration: 2,
-                stagger: 0.1
-            })
-            .from(segment.querySelector('.user-agreement-text__information'), {
-                opacity: 0,
-                xPercent: -10,
-                duration: 1,
-                stagger: 0.2
-            });
-    })
 });
 
 // Another type of animation
@@ -55,24 +43,3 @@ document.addEventListener('DOMContentLoaded', () => {
 //             duration: 1
 //         });
 // });
-
-// Previous version
-// const refactored = gsap.timeline();
-// refactored
-//     .from('.user-agreement-text__header', {
-//         opacity: 0,
-//         yPercent: -15,
-//         duration: 1
-//     })
-//     .from('.user-agreement-text__paragraph', {
-//         opacity: 0,
-//         xPercent: -10,
-//         duration: 2,
-//         stagger: 0.1
-//     })
-//     .from('.user-agreement-text__information', {
-//         opacity: 0,
-//         xPercent: -10,
-//         duration: 1,
-//         stagger: 0.2
-//     });
