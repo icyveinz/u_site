869a4a738978dd5837ad4616e74471ab9323c57b
rust_lang_lover <rust_lang_lover@proton.me>
2024-08-01 13:34:12 +0300

    added new animation to legal_gsap_animation.js

--- src/js/js_scripts/legal_gsap_animation.js
+++ src/js/js_scripts/legal_gsap_animation.js
@@ -1,23 +1,37 @@
+gsap.registerPlugin(ScrollTrigger);
+
 document.addEventListener('DOMContentLoaded', () => {
-    const refactored = gsap.timeline();
-    refactored
-        .from('.user-agreement-text__header', {
-            opacity: 0,
-            yPercent: -15,
-            duration: 1
-        })
-        .from('.user-agreement-text__paragraph', {
-            opacity: 0,
-            xPercent: -10,
-            duration: 2,
-            stagger: 0.1
-        })
-        .from('.user-agreement-text__information', {
-            opacity: 0,
-            xPercent: -10,
-            duration: 1,
-            stagger: 0.2
+    gsap.from('.user-agreement-text__header', {
+        opacity: 0,
+        yPercent: -15,
+        duration: 1
+    });
+    const segment_trigger = document.querySelectorAll('.user-agreement-additional-layout');
+    segment_trigger.forEach((segment) => {
+        const single_timeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: segment,
+                start: 'top bottom',
+                scrub: true,
+                markers: true,
+                end: 'bottom bottom',
+                toggleActions: 'play none none reverse'
+            }
         });
+        single_timeline
+            .from(segment.querySelector('.user-agreement-text__paragraph'), {
+                opacity: 0,
+                xPercent: -10,
+                duration: 2,
+                stagger: 0.1
+            })
+            .from(segment.querySelector('.user-agreement-text__information'), {
+                opacity: 0,
+                xPercent: -10,
+                duration: 1,
+                stagger: 0.2
+            });
+    })
 });
 
 // Another type of animation
