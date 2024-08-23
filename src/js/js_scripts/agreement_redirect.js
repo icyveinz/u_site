ce5626962664ca91bc8b6d0e50e9f77d117a9411
rust_lang_lover <rust_lang_lover@proton.me>
2024-08-23 12:23:39 +0300

    // check point

--- src/js/js_scripts/agreement_redirect.js
+++ src/js/js_scripts/agreement_redirect.js
@@ -1,5 +1,5 @@
 $(function() {
     $('.return-to-main-button').on('click', function() {
-        window.location.replace('/presentation')
+        window.location.replace('/')
     })
 })
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+    entry: {
+        main: [
+            "./src/js/js_scripts/age_verification_main.js",
+            "./src/js/js_scripts/class_holder.js",
+            "./src/js/js_scripts/visual_activators.js",
+            "./src/js/js_scripts/hovered-links-footer.js",
+            "./src/js/js_scripts/network_worker.js",
+            "./src/js/js_scripts/main_styler.js",
+            "./src/js/js_scripts/burger_menu.js",
+            "./src/js/js_scripts/walgreen-redirect.js"
+        ],
+        user_agreement: [
+            "./src/js/js_scripts/check_if_age_confirmed.js",
+            "./src/js/js_scripts/visual_activators.js",
+            "./src/js/js_scripts/hovered-links-footer.js",
+            "./src/js/js_scripts/burger_menu.js",
+            "./src/js/js_scripts/agreement_redirect.js",
+            "./src/js/js_scripts/legal_gsap_animation.js"
+        ]
+    },
+    output: {
+        filename: 'bundled_[name].js',
+        path: path.resolve(__dirname, 'dist/js/js_scripts'),
+    }
+};
+
+// npx webpack to run
