cb53ed2cfb210e8466eab1e79901028ec0c39e80
rust_lang_lover <rust_lang_lover@proton.me>
2024-08-23 15:33:14 +0300

    // changed meta tags in user_agreement index.html + removed unused libraries

--- src/js/js_scripts/legal_gsap_animation.js
+++ src/js/js_scripts/legal_gsap_animation.js
@@ -1,5 +1,3 @@
-gsap.registerPlugin(ScrollTrigger);
-
 document.addEventListener('DOMContentLoaded', () => {
     const legal_line = gsap.timeline();
     legal_line
@@ -21,25 +19,3 @@ document.addEventListener('DOMContentLoaded', () => {
             stagger: 0.2
         });
 });
-
-// Another type of animation
-// gsap.from('.user-agreement-text__header', {
-//     opacity: 0,
-//     yPercent: -15,
-//     duration: 1
-// });
-// const listing = document.querySelectorAll('.user-agreement-additional-layout');
-// listing.forEach((value) => {
-//     const timeline = gsap.timeline();
-//     timeline
-//         .from(value.querySelector('h4'), {
-//             opacity: 0,
-//             xPercent: -10,
-//             duration: 2
-//         })
-//         .from(value.querySelector('h5'), {
-//             opacity: 0,
-//             xPercent: -10,
-//             duration: 1
-//         });
-// });
