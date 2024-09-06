9fadedcdaf3ceca13b54f91de63ee193614f794b
Egor Ivanov <rust_lang_lover@proton.me>
2024-09-07 00:16:20 +0300

    // added gsap + text-plugin lib in the project

---
+++ src/js/js_scripts/top_page_presentation_gsap.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    const presentation_nodes = document.querySelectorAll('.picture-text-grid');
+    presentation_nodes.forEach((node, index) => {
+        if ((index+=1) % 2 !== 0) {
+            const odd_timeline = initTimeline(node);
+            odd_timeline
+                .from(node.querySelector('.picture-text-grid__text-right-oriented__image-container'), {
+                    opacity: 0,
+                    yPercent: -20
+                })
+                .from(node.querySelector('.picture-text-grid__text-contents__header'), {
+                    opacity: 0,
+                    xPercent: 40
+                })
+                .from(node.querySelectorAll('.picture-text-grid__text-contents__listing__li'), {
+                    opacity: 0,
+                    yPercent: 40,
+                    stagger: 0.3
+                })
+        }
+        else {
+            const even_timeline = initTimeline(node);
+            even_timeline
+                .from(node.querySelector('.picture-text-grid__text-left-oriented__image-container'), {
+                    opacity: 0,
+                    yPercent: -20
+                })
+                .from(node.querySelector('.picture-text-grid__text-contents__header'), {
+                    opacity: 0,
+                    xPercent: -40
+                })
+                .from(node.querySelectorAll('.picture-text-grid__text-contents__listing__li'), {
+                    opacity: 0,
+                    yPercent: 40,
+                    stagger: 0.3
+                })
+        }
+    })
+});
+
+function initTimeline(node) {
+    return gsap.timeline({
+        scrollTrigger: {
+            trigger: node,
+            start: 'top bottom-=20%',
+            scrub: true,
+            end: 'bottom bottom-=20%',
+            markers: true
+        }
+    })
+}
